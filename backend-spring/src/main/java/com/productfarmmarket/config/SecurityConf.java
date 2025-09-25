b3301192a71e059b772371ecd02d908e225c0ee7
Vitalii Maletych <124867918+v-maletych@users.noreply.github.com>
2025-09-25 11:30:01 +0300

    Backend:
    1. Add to Dockerfile possibility create .jar file immediately.
    2. Add @Column and @Table to forward models: Category, Order, OrderItem, Product, Review, Role, User, Wishlist.
    3. Create new config file "SecurityConf" in package com.productfarmmarket.com.config and add permitAll to check connection with DB.
    4. Additionally DB is created with tables and columns but without fields.

--- backend-spring/src/main/java/com/productfarmmarket/config/SecurityConf.java
+++ backend-spring/src/main/java/com/productfarmmarket/config/SecurityConf.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConf {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
